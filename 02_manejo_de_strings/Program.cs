fd67303929194346fd53ad5bb69a84379a20876f
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-07-03 09:33:49 -0500

    feat: se ingresaron metodos a manejo de strings

--- 02_manejo_de_strings/Program.cs
+++ 02_manejo_de_strings/Program.cs
@@ -48,8 +48,18 @@ Console.WriteLine();
 bool sonIgualesPasivamente = texto1.Equals(texto2, StringComparison.OrdinalIgnoreCase);
 Console.WriteLine($"¿Texto1 y Texto2 son iguales? {sonIgualesPasivamente}");
 
-// Eliminación de espacios en blanco al principio y al final de las cadenas
+// Validar si un string esta vacio o null
+Console.WriteLine();
+bool esVacio = string.IsNullOrEmpty(string.Empty);
+bool esNull = string.IsNullOrEmpty(null);
+Console.WriteLine($"¿Texto vacío o null? {esVacio} - {esNull}");
 
+// Validar si un string esta vacio,null o lleno de espacios
+Console.WriteLine();
+bool esVacioConEspacios = string.IsNullOrWhiteSpace(multiplesCaracteres);
+Console.WriteLine($"¿Texto con espacios vacío? {esVacioConEspacios}");
+
+// Eliminación de espacios en blanco al principio y al final de las cadenas
 Console.WriteLine();
 string texto1SinEspacios = texto1.Trim();
 string texto2SinEspacios = texto2.Trim();
@@ -57,7 +67,6 @@ Console.WriteLine($"Texto1 sin espacios en blanco: {texto1SinEspacios}");
 Console.WriteLine($"Texto2 sin espacios en blanco: {texto2SinEspacios}");
 
 // Reemplazo de caracteres en las cadenas
-
 Console.WriteLine();
 string texto1Reemplazado = texto1.Replace("obsesión", "lucha");
 string texto2Reemplazado = texto2.Replace("lucha", "obsesión");
@@ -65,7 +74,6 @@ Console.WriteLine($"Texto1 reemplazado: {texto1Reemplazado}");
 Console.WriteLine($"Texto2 reemplazado: {texto2Reemplazado}");
 
 // Invertir cadenas
-
 Console.WriteLine();
 string texto1Invertido = new string(texto1.Reverse().ToArray());
 string texto2Invertido = new string(texto2.Reverse().ToArray());
@@ -73,7 +81,6 @@ Console.WriteLine($"Texto1 invertido: {texto1Invertido}");
 Console.WriteLine($"Texto2 invertido: {texto2Invertido}");
 
 // Cambiar el caso de las letras (convertir en mayusculas o minusculas)
-
 Console.WriteLine();
 string texto1Mayusculas = texto1.ToUpper();
 string texto2Minusculas = texto2.ToLower();
@@ -81,7 +88,6 @@ Console.WriteLine($"Texto1 en mayúsculas: {texto1Mayusculas}");
 Console.WriteLine($"Texto2 en minúsculas: {texto2Minusculas}");
 
 // Buscar una cadena en otra
-
 Console.WriteLine();
 int indiceTexto1 = texto1.IndexOf("mejor");
 int indiceTexto2 = texto2.IndexOf("mejor");
@@ -89,7 +95,6 @@ Console.WriteLine($"Indice de 'mejor' en texto1: {indiceTexto1}");
 Console.WriteLine($"Indice de 'mejor' en texto2: {indiceTexto2}");
 
 // Cortar cadenas en partes
-
 Console.WriteLine();
 string[] divisionTexto1 = texto1.Split(' '); // Dividir texto1 en palabras
 string[] divisionTexto2 = texto2.Split(' '); // Dividir texto2 en palabras
